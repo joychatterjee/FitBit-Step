2defc503d6a0dd1c3932286e5af5e21ae87e08e5
Joy Chatterjee <chatterjee.joy2@gmail.com>
2016-10-01 14:32:07 +0530

    Add files via upload

---
+++ restcall.js
@@ -0,0 +1,26 @@
+// restcall.js - module for handling REST API calls
+
+// METHOD == GET
+/*eslint-env node */
+exports.get = function(options, secure, callbackFn) {
+
+  var httpcall = require(secure ? "https" : "http");
+
+  var req = httpcall.request(options, function(res) {
+    res.setEncoding("utf-8");
+    var responseString = "";
+
+    res.on("data", function(data) {
+      responseString += data;
+    });
+
+    res.on("end", function() {
+      var responseObject = JSON.parse(responseString);
+      callbackFn(responseObject);
+    });
+  });
+
+  req.write(""); //method == GET, no data
+  req.end();
+
+};
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+
+// GET initial page
+/*eslint-env node */
+exports.index = function(req, res){
+  res.render("index");
+};
